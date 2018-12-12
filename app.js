6befa39f2f05b0a1b2710ed6aa680f2869430326
Quinn Loots <loots.quinn@gmail.com>
2018-12-12 12:29:18 +0100

    exercise 1 dom

---
+++ app.js
@@ -0,0 +1,20 @@
+let bodyRef = document.body;
+bodyRef.classList.remove("bg-aqua");
+bodyRef.classList.add("bg-olive");
+
+let alineaRef = document.getElementById("first-paragraph");
+alineaRef.classList.remove("bg-lime");
+alineaRef.classList.remove("gray");
+alineaRef.classList.add("aqua");
+
+let silverRef = document.getElementsByClassName("bg-silver");
+silverRef.classList.add("bg-teal");
+silverRef.classList.remove("bg-silver");
+
+let quote = document.getElementsByTagName("blockquote");
+quote.classList.add("bg-white");
+
+
+
+
+
