466ef90e1aaf369043f73ce319baa77173e50621
Quinn Loots <loots.quinn@gmail.com>
2018-12-12 16:39:33 +0100

    make dom exercises

--- app.js
+++ app.js
@@ -7,17 +7,81 @@ alineaRef.classList.remove("bg-lime");
 alineaRef.classList.remove("gray");
 alineaRef.classList.add("aqua");
 
+
 let silverRef = document.getElementsByClassName("bg-silver");
-silverRef.classList.add("bg-teal");
-silverRef.classList.remove("bg-silver");
+
+for (let i = 0; i < silverRef.length; i++) {
+    silverRef[i].classList.add("bg-teal");
+    silverRef[i].classList.remove("bg-silver");
+}
+
 
 let quote = document.getElementsByTagName("blockquote");
-quote.classList.add("bg-white");
+for (let i = 0; i < quote.length; i++) {
+   quote[i].classList.add("bg-white");
+}
 
-let query = document.querySelector("my-table");
+
+let query = document.querySelector("#my-table");
 query.classList.add("bg-purple");
 var para = document.querySelectorAll("p");
-para.classList.add("shadow");
+for (let i = 0; i < para.length; i++) {
+    para[i].classList.add("shadow");
+}
+
+
+
+let pres = document.getElementsByTagName("pre");
+for (let i = 0; i < pres.length; i++) {
+    pres[i].style.color = "red";
+    pres[i].style.backgroundColor = "blue";
+    pres[i].style.borderBottom = "3px solid red";
+
+}
+
+let heads = document.getElementsByTagName("h3")[0];
+heads.innerHTML="<em>Italic title ! yeah !</em>";
+
+
+
+let headers = document.getElementsByTagName("h2")[0];
+headers.innerHTML="<strong>HTML doesn't work !</strong>";
+
+let list = document.getElementsByTagName("ul")[0];
+let li = document.createElement("li");
+  li.innerHTML=("My best friend is <a href='http://www.google.com'>Google</a>");
+  list.appendChild(li);
+
+  var link = document.querySelectorAll("li a")[0];
+  link.style.color="green";
+
+let liste = document.getElementsByTagName("ol")[0];
+let kinderen = liste.children;
+let lengte = kinderen.length;
+for (var i = 0; i < lengte; i++) {
+    liste.removeChild(kinderen[0]);
+}
+
+
+
+let array =  ["Silent Teacher","Code Monkey", "CodeCombat"];
+for (var i = 0; i < array.length; i++) {
+    let entry = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = array[i];
+    entry.appendChild(a);
+    liste.appendChild(entry);
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
